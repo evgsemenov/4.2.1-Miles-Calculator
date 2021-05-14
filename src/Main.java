9d857654808709c65ee69398cafc1d9ad3b4cb6f
Evgenii Semenov <eo.semenoff@gmail.com>
2021-05-14 13:19:31 +0300

    Miles calculator added

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 14897;
+        int rublePerMile = 20;
+        int milesTotal = (int) (ticketCost / rublePerMile);
+        System.out.println("Миль начислено:" + milesTotal);
+
+    }
+}
+
